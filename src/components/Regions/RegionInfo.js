70ab5a20da9c8937d5b79fe007609b926c9dc950
Patrick Kennefod Annan <100440561+Kenneford@users.noreply.github.com>
2022-08-31 01:03:42 +0200

    Still building data information

--- src/components/Regions/RegionInfo.js
+++ src/components/Regions/RegionInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import allData from "../../data.json";
 import { useParams } from "react-router-dom";
+import "./RegionInfo.css";
+import { NavLink, Link, Outlet } from "react-router-dom";
 
 export default function NewRegionInfo() {
   const { id } = useParams();
@@ -8,14 +10,41 @@ export default function NewRegionInfo() {
   return (
     <div>
       {allData.map((data) => (
-        <div>
-          <p> {data.regions[id].name}</p>
-          <p> {data.regions[id].info}</p>
-          <p>
-            {data.regions[id].places.map((place) => (
-              <p>{place.name}</p>
-            ))}
-          </p>
+        <div key={data.id}>
+          <div
+            className="accraBgImage"
+            style={{
+              backgroundImage: `url(${data.regions[id].bgImage})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              height: "450px",
+            }}
+          >
+            <h1> {data.regions[id].name}</h1>
+            <p>({data.regions[id].location})</p>
+          </div>
+          <div className="regContWrap">
+            <h2>A bit about Greater Accra Region</h2>
+            <p> {data.regions[id].info}</p>
+            <div>
+              <h2>Hotels in Accra</h2>
+              <h2>Places to shop</h2>
+            </div>
+            <div>
+              <h2>Top restaurants</h2>
+            </div>
+            <div>
+              <h2>Entertainments</h2>
+            </div>
+            {/* <div className="accraAttractions">
+              <p className="popLinks">Popular Links 👉🏾</p>
+              {data.regions[id].places.map((place) => (
+                <p>
+                  <Link to={`/tour-place-info/${id}`}>{place.name}</Link>
+                </p>
+              ))}
+            </div> */}
+          </div>
         </div>
       ))}
     </div>
--- src/components/pages/TourInfo.js
+++ src/components/pages/TourInfo.js
@@ -5,17 +5,5 @@ import { useParams } from "react-router-dom";
 export default function TourInfo() {
   const params = useParams();
   console.log("params", params);
-  return (
-    <div>
-      {allData.map((data) => (
-        <div>
-          {data.regions.map((region) => (
-            <div>
-              <p>{region.places[params.id].name}</p>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
-  );
+  return <div>TourInfo</div>;
 }
